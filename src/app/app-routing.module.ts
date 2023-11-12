14e047064d78d50931df921d18cdba26369c9c85
ABHIJITH P A <aputhan@ibizsoftinc.com>
2023-11-12 18:30:35 +0530

    111

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { LoginComponent } from './component/login/login.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'login', component:LoginComponent}
+  {path:'login', component:LoginComponent},
+  {path:'profile', component:ProfileComponent}
 ];
 
 @NgModule({
